9ab5103e70ad2888b124ad67cf5b99532f894715
rasoanirinamialisoa <hei.lisa.30@gmail.com>
2025-07-14 22:56:08 +0300

    feat(models/migration): create models prompt

---
+++ database/migrations/2025_07_14_194842_create_prompts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('prompt', function (Blueprint $table) {
+            $table->id();
+            $table->text('content');
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+
+            $table->unsignedBigInteger('campaign_id');
+            $table->foreign('campaign_id')
+                ->references('id')
+                ->on('campaign');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prompt');
+    }
+};
