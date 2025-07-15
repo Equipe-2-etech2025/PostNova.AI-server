f0c31fe6bd37ec307fb6740067de426c70d96119
rasoanirinamialisoa <hei.lisa.30@gmail.com>
2025-07-15 22:41:12 +0300

    feat(factory-seeder): add factory & seeder for generate data Feature

--- database/factories/FeatureFactory.php
+++ database/factories/FeatureFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Social>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
  */
-class SocialFactory extends Factory
+class FeatureFactory extends Factory
 {
     /**
      * Define the model's default state.
