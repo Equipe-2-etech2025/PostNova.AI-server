bb19f775ea7f9c12be18a59ceaad3797f5f3068e
rasoanirinamialisoa <hei.lisa.30@gmail.com>
2025-07-16 14:13:41 +0300

    feat(factory-seeder): add factory & seeder for generate data Prompt

---
+++ database/factories/PromptFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Prompt;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Prompt>
+ */
+class PromptFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    protected $model = Prompt::class;
+
+    public function definition(): array
+    {
+        return [
+            'content' => fake()->paragraph(),
+            'campaign_id' => Campaign::inRandomOrder()->first()?->id ?? Campaign::factory(),
+            'created_at' => now(),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $this->call([
+            PromptSeeder::class,
             CampaignSeeder::class,
             TypeCampaignSeeder::class,
             FeatureSeeder::class,
