618c1a09aebd7c9670f1848904b670ffe94dc0ef
nathanJuanito <154326723+nathanJuanito@users.noreply.github.com>
2025-07-18 10:48:11 +0300

    integ auth back
    cors,migrations,routes,sessions

--- app/Http/Controllers/API/Auth/AuthController.php
+++ app/Http/Controllers/API/Auth/AuthController.php
@@ -90,7 +90,7 @@ class AuthController extends Controller
 
             return response()->json([
                 'success' => false,
-                'message' => 'Erreur lors de la connexion.',
+                'message' => $e->getMessage(),
             ], 401);
         }
     }
