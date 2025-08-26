63997da844429c2b672024a115ea2a8af9f13a80
nathanJuanito <154326723+nathanJuanito@users.noreply.github.com>
2025-08-26 15:05:37 +0300

    Update CampaignInteractionServiceInterface.php

--- app/Services/Interfaces/CampaignInteractionServiceInterface.php
+++ app/Services/Interfaces/CampaignInteractionServiceInterface.php
@@ -19,8 +19,8 @@ interface CampaignInteractionServiceInterface
 
     public function getInteractionsByCriteria(array $criteria);
 
-    // Méthodes spécifiques aux interactions
-    public function update(int $id, CampaignInteractionDto $dto): CampaignInteraction;
+    // // Méthodes spécifiques aux interactions
+    // public function update(int $id, CampaignInteractionDto $dto): CampaignInteraction;
 
     public function getInteractionsByCampaignId(int $campaignId);
 
