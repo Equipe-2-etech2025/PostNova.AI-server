b1f6bd939d0f09c5e396fe43c1b0542e643aba07
nathanJuanito <154326723+nathanJuanito@users.noreply.github.com>
2025-08-28 15:28:00 +0300

    Update CampaignInteractionResource.php

--- app/Http/Resources/CampaignInteraction/CampaignInteractionResource.php
+++ app/Http/Resources/CampaignInteraction/CampaignInteractionResource.php
@@ -17,11 +17,6 @@ class CampaignInteractionResource extends JsonResource
             'shares' => $this->resource->shares,
             'created_at' => $this->resource->created_at?->toIso8601String(),
             'updated_at' => $this->resource->updated_at?->toIso8601String(),
-            'links' => [
-                'self' => route('api.campaign-interactions.show', $this->resource->id),
-                'campaign' => $this->resource->campaign_id ? route('api.campaigns.show', $this->resource->campaign_id) : null,
-                'user' => $this->resource->user_id ? route('api.users.show', $this->resource->user_id) : null,
-            ],
         ];
     }
 }
